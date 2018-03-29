82de179f192235b7245fcee0dd8c472fa4ee9b7e
Adrian Amaglio <Vous@exemple.com>
2018-03-29 15:51:01 +0200

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Article managment
+Offer an API and a nice web interface to manage article redaction workflow.
+This tool aim to be simple to use or configure for quick installation but customizable for needy organizations.
+
+## Customize the publication
+Your validated articles can end wherever you want. They can be left in the database, sent by e-mail, sent on another API…
+You can always correct them and send them again ! Your endpoint must support this operation.
+
+## Customize workflow
+Add custom steps to the article workflow. Defaults are :
+- Creation
+- Redaction
+- Correction
+- Integration
+Article also have event or time based actions !
+
+## Customize the constraints
+Each article can have its length constraint
+
+## Customize the users
+Give them right to validate any step in the workflow
+
+## Work together
+Integration with etherpad for group work is on the TODO list
---
+++ create.postgre.sql
@@ -0,0 +1,56 @@
+create table steps (
+  id serial primary key,
+  name varchar(100),
+  parent_step int default null,
+  
+  foreign key (parent_step) references steps (id),
+);
+
+create table users (
+  id serial primary key,
+  username varchar(255),
+  password varchar(255) default null
+);
+
+create table groups (
+  id serial primary key,
+  name varchar(100)
+);
+
+-- who have the rights
+create table steps_groups (
+  group_id int,
+  step_id int,
+  can_read boolean,
+  can_write boolean,
+  can_create boolean,
+  can_delete boolean,
+  can_validate boolean,
+
+  foreign key (group_id) references groups (id),
+  foreign key (step_id) references steps (id)
+);
+
+-- who can manage rights
+create table groups_groups (
+  admin_id int,
+  group_id int,
+
+  foreign key (admin_id) references groups (id),
+  foreign key (group_id) references groups (id),
+);
+
+create table articles (
+  id serial primary key,
+  title varchar(255),
+  content text,
+  step_id int,
+  max_char int default 0,
+  min_char int default 0,
+  author varchar(100),
+  
+  foreign key (step_id) references steps (id)
+);
+
+insert into users(username) values('guest');
+insert into users(username) values('admin');
