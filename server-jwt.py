8e1fa56f5bef23cb6f8530419bc1cf0fedefca89
Adrian Amaglio <Vous@exemple.com>
2018-07-15 15:32:46 +0200

    awe

--- server-jwt.py
+++ server-jwt.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
 from flask_restful import Api, Resource
-import psycopg2
+#import psycopg2
 
 
 ###############################################################################
