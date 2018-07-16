a0dd9f4aeddfb76cb3371cab99a88390163914c9
Adrian Amaglio <Vous@exemple.com>
2018-07-16 20:21:12 +0200

    clean

--- create.postgre.sql
+++ create.postgre.sql
@@ -7,6 +7,7 @@ create  table steps_steps (
   id serial primary key,
   step integer,
   next integer,
+  caption varchar(100),
   foreign key(step) references steps(id),
   foreign key(next) references steps(id)
 );
@@ -42,6 +43,19 @@ insert into steps(id, name) values(2, 'Integration');
 insert into steps(id, name) values(3, 'Archive');
 insert into steps(id, name) values(10, 'Émission');
 insert into steps(id, name) values(11, 'Archive radio');
+
+insert into steps_steps(step, next, caption) values(0, 1, 'Envoyer en relecture');
+insert into steps_steps(step, next, caption) values(1, 0, 'Renvoyer en rédaction');
+insert into steps_steps(step, next, caption) values(1, 2, 'Envoyer en mise en page');
+insert into steps_steps(step, next, caption) values(2, 0, 'Renvoyer en rédaction');
+insert into steps_steps(step, next, caption) values(2, 1, 'Renvoyer en relecture');
+insert into steps_steps(step, next, caption) values(2, 3, 'Archiver l’article');
+insert into steps_steps(step, next, caption) values(3, 0, 'Renvoyer en rédaction');
+insert into steps_steps(step, next, caption) values(3, 1, 'Renvoyer en relecture');
+insert into steps_steps(step, next, caption) values(3, 2, 'Renvoyer en mise en page');
+
+insert into steps_steps(step, next, caption) values(10, 11, 'Archiver l’émission');
+insert into steps_steps(step, next, caption) values(11, 10, 'Renvoyer l’émission');
 
 insert into articles(title, format, type, due_date, content, exergue, step_id, max_lenght, min_lenght, author) values ('Il y a 30 ans', 'Edito', 'article', '2018-07-15 21:26:00', 'blablabla', 'bla', 0, 100, 30, 'lucie');
 insert into articles(title, format, type, due_date, content, exergue, step_id, max_lenght, min_lenght, author) values ('Il y a 30 heures', 'Concert', 'article', '2018-07-15 21:26:00', 'bla2blabla', 'bla2', 0, 100, 30, 'Rose');
--- server-jwt.py
+++ server-jwt.py
@@ -30,25 +30,6 @@ class User():
     def __str__(self):
         return "User(id='%s')" % self.id
 
-users = [
-    User(0, 'guest', ''),
-    User(1, 'mc', 'mc'),
-    User(2, 'scribus', 'scribus'),
-]
-steps = [
-
-{'id':0, 'name':'Rédaction', 'next_steps':{1:'Envoyer en relecture'}},
-{'id':1, 'name':'Correction', 'next_steps':{2:'Envoyer en mise en page' ,0:'Renvoyer en rédaction'}},
-{'id':2, 'name':'Integration', 'next_steps':{3:'Archiver l’article', 1:'Renvoyer en relecture', 0:'Renvoyer en rédaction'}},
-{'id':3, 'name':'Archive', 'next_steps':{3:'Archiver l’article', 1:'Renvoyer en relecture', 0:'Renvoyer en rédaction'}},
-{'id':10,'name': 'Émission', 'next_steps':{11:'Archiver'}},
-{'id':11,'name': 'Archive radio', 'next_steps':{}},
-]
-
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
-steps_table = {u['id']: u for u in steps}
-
 def authenticate(username, password):
     user = username_table.get(username, None)
     if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
@@ -58,8 +39,33 @@ def identity(payload):
     user_id = payload['identity']
     return userid_table.get(user_id, None)
 
+###############################################################################
+#                   development variables
+###############################################################################
+
+users = [
+    User(0, 'guest', ''),
+    User(1, 'mc', 'mc'),
+    User(2, 'scribus', 'scribus'),
+]
+steps = [
+  {'id':0, 'name':'Rédaction', 'next_steps':{1:'Envoyer en relecture'}},
+  {'id':1, 'name':'Correction', 'next_steps':{2:'Envoyer en mise en page' ,0:'Renvoyer en rédaction'}},
+  {'id':2, 'name':'Integration', 'next_steps':{3:'Archiver l’article', 1:'Renvoyer en relecture', 0:'Renvoyer en rédaction'}},
+  {'id':3, 'name':'Archive', 'next_steps':{2:'Renvoyer l’article', 1:'Renvoyer en relecture', 0:'Renvoyer en rédaction'}},
+  {'id':10,'name': 'Émission', 'next_steps':{11:'Archiver'}},
+  {'id':11,'name': 'Archive radio', 'next_steps':{}},
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+steps_table = {u['id']: u for u in steps}
+
+###############################################################################
+#                   Flask configuration
+###############################################################################
+
 app = Flask(__name__)
-app.debug = True
 app.config['SECRET_KEY'] = 'Ìè"ÌRAÛÏ±\Ô95BME;ýG¾Ð¨ÈºÕ3n×ð?pºét)ëXbZ¨¢ê;é&®ÿxq$Æ×.è³å[Mð²xoF»¨î´©dFC¼ÿrÏ4=mX¹{T5Ù«ÑUG¯L#P¦z¬úo±xÓóó»pÐsNC<¹W3úNn&He%n8Þc÷Âä7b(M,ÌD(ËÊäcéÅ~#·ÇÂ/í5øa-³°rÈ3èCã¿bBF¹ÉjÝHü"²¬.nq`Ý¿2ú²¼^¶z)Û;Ëå¸^béWÂìêX!h5¸(Û÷v9·"ø:¨½â>RtÐé"ØÕõÚ¹:E¢Ëàú5xÙ³Þ³RÓ<¡«ÛÖ°oÕeÔºK)àuÈ©8¨b¦¾½;ÍëU£÷oa«âÈv`¯Ftüû3ãÄïG/%"¿¼÷GÒÎ>#SJ§Õ^A@aÀy´!U-Ê°]ª4á}]ðÜê}A÷íÚ»Äv¢yxÓ¤ë#Æoÿóg±p¶°caTs½¶áVr½ÑN`aÐ¸ÊyW°TS6Suø{¬)íEøAëßëäÍÙ!¼F@FÍ*·Û°Ïßó'
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
@@ -80,21 +86,31 @@ class ArticleAPI(Resource):
     return cur.fetchone()
 
   def put(self, id):
+      cur.execute("""update articles SET (title={:s}, format={:s}, type={:s}, due_date={:s}, content={:s}, exergue={:s}, step_id={:d}, max_lenght={:d}, min_lenght={:d}, author={:s})""".format()) 
     return 'put '+str(id)
+
+  def post(self):
+    cur.execute("""insert into articles(title, format, type, due_date, content, exergue, step_id, max_lenght, min_lenght, author) values ({}, {}, {}, {}, {}, {})""".format()).
+    return 'post '+str(id)
 
 
 class ArticleListAPI(Resource):
   def get(self, step_id):
+    cur.execute("select * from steps where id={:d}".format(step_id))
+    current_step=cur.fetchone()
+
+    cur.execute("select next,caption from steps_steps where step={:d}".format(step_id))
+    next_steps=cur.fetchall()
+
     cur.execute("""select * from articles where step_id={:d}""".format(step_id))
     return {
       "deadline_colors" : [
         {"seconds" : 3600, "color" : "red"},
         {"seconds" : 14400, "color" : "orange"}
       ],
-      "step" : {
-        "id":step_id,
-        "next_steps":steps_table[step_id]['next_steps'],
-        "name":steps_table[step_id]['name'],
+      "step" : current_step,
+      "next_steps":next_steps,
+      "autorizations": {
         "can_create": False,
         "can_edit": True,
         "can_admin" : False,
@@ -117,10 +133,13 @@ class ArticleListAPI(Resource):
       "articles" : cur.fetchall()
     }
 
+###############################################################################
+#                   Other flask configuration
+###############################################################################
+
 api_version = 'v1.0'
 api.add_resource(ArticleAPI, '/'+api_version+'/article/<int:id>', endpoint = 'article')
 api.add_resource(ArticleListAPI, '/'+api_version+'/articles/<int:step_id>', endpoint = 'articles')
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
